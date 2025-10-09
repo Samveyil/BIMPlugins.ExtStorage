3fdb3d310b626178d7ece6510ca28a728ae23644
Samir Shibliev <samir.shibliev01@mail.ru>
2025-10-09 16:59:43 +0300

    big update

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
 # BIMPlugins.ExtStorage
+
+Package with my developments
+
+# Extensions
+
+## ElementIdExtensions
+
+```C#
+var wall = new ElementId(123456).ToElement<Wall>();
+var parameterFilter = sheetParameter.Id.CreateEqualsFilter("ÊÆ3"); 
+```
+
+## ElementIdExtensions
+
+```C#
+var wall = new ElementId(123456).ToElement<Wall>();
+var parameterFilter = sheetParameter.Id.CreateEqualsFilter("ÊÆ3"); 
+```
+
+## ElementExtensions
+
+```C#
+var wallType = wall.ToElementType<WallType>();
+var bic = wall.GetBuiltInCategory(); 
+```
+
+## DocumentExtensions
+
+```C#
+var walls = doc.ToElements(BuiltInCategory.OST_Walls);
+var floors = doc.ToElements<Floor>(); 
+```
+
+## SystemExtensions
+
+```C#
+var roundedValue = parameter.AsDouble().Round(5);
+var isEmpty = parameter.AsString().IsNullOrEmpty();
+var filePath = directoryPath.AppendPath(fileName);
+```
+
+# Methods
+
+## ParameterMethods
+
+UnitType no mater of Revit version
+"mm",
+"cm",
+"m",
+"m2",
+"m3",
+"general",
+"degrees",
+"degreesMinutes",
+"W",
+"V",
+
+```C#
+var areaToInt = UnitUtils.ConvertToInternalUnits(area, ParameterMethods.GetUnitType("m2"));
+```
+
+# RevitAPI
+
+Get active document, application, view whenever you want
+
+```C#
+var uiapp = RevitAPI.UIApplication;
+var app = RevitAPI.Application;
+var uidoc = RevitAPI.UIDocument;
+var doc = RevitAPI.Document;
+var view = RevitAPI.ActiveView;
+```
--- build/Build.cs
+++ build/Build.cs
@@ -1,86 +1,22 @@
+using BIMPlugins.Nuke;
 using Nuke.Common;
-using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
-using Nuke.Common.Tools.DotNet;
-using Nuke.Common.Tools.MSBuild;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using static Nuke.Common.Tools.DotNet.DotNetTasks;
-using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
 
-class Build : NukeBuild
+public class Build : BIMPluginsBuild
 {
+    protected override int MajorVersion => 0;
+    protected override int MinorVersion => 1;
+    protected override int MaintenanceVersion => 9;
+
+
+    [Solution("BIMPlugins.ExtStorage.sln")]
+    public override Solution Solution { get; }
+
+    [Parameter("Project to build name")]
+    public override string ProjectName { get; } = "BIMPlugins.ExtStorage";
+
+    [Parameter("Build configurations to run sequentially")]
+    public override Configuration[] Configurations { get; } = new[] { Configuration.R2019, Configuration.R2020, Configuration.R2021, Configuration.R2022, Configuration.R2023, Configuration.R2024 };
+
     public static int Main() => Execute<Build>(x => x.Compile);
-
-    [Solution] readonly Solution Solution;
-
-    [Parameter("Name of the plugin to compile")] readonly string PluginName = "BIMPlugins.ExtStorage";
-
-    [Parameter("URL of the NuGet feed")] readonly string NugetSource = @"https://nuget.pkg.github.com/Samveyil/index.json";
-    [Parameter("API Key for the NuGet feed")] readonly string NugetApiKey = "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";
-
-    Target Compile => _ => _
-        .Executes(() =>
-        {
-            var project = Solution.GetProject(PluginName);
-            if (project == null)
-                throw new FileNotFoundException("Not found!");
-
-            var matchedNupkgs = Directory
-                .GetFiles(RootDirectory, "*.nupkg", SearchOption.AllDirectories)
-                .ToList();
-
-            foreach (var nupkg in matchedNupkgs)
-            {
-                File.Delete(nupkg);
-            }
-
-            var build = new List<string>();
-            foreach (var (_, c) in project.Configurations)
-            {
-                var configuration = c.Split("|")[0];
-                var platform = c.Split("|")[1];
-
-                if (configuration.Contains("Debug") || configuration == "Release" || build.Contains(configuration))
-                    continue;
-
-                build.Add(configuration);
-
-                Serilog.Log.Debug($"Configuration: {configuration}");
-
-                string msbuildPath = @"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe";
-
-                MSBuild(_ => _
-                    .SetProcessToolPath(msbuildPath)
-                    .SetProjectFile(project.Path)
-                    .SetConfiguration(configuration)
-                    .SetTargets("Restore"));
-
-                MSBuild(_ => _
-                    .SetProcessToolPath(msbuildPath)
-                    .SetProjectFile(project.Path)
-                    .SetConfiguration(configuration)
-                    .SetTargets("Rebuild"));
-            }
-        });
-
-    Target PushToGitHubNugetRepository => _ => _ 
-    .Executes(() =>
-        {
-            var matchedNupkgs = Directory
-                .GetFiles(RootDirectory, "*.nupkg", SearchOption.AllDirectories)
-                .ToList();
-
-            foreach (var nupkg in matchedNupkgs)
-            {
-                DotNetNuGetPush(s => s
-                    .SetTargetPath(nupkg)
-                    .SetApiKey(NugetApiKey)
-                    .SetSource(NugetSource)
-                );
-            }
-        });
 }
