6f522fc8173a52eeda4fc9647228d1ad7a5a57e2
Samir Shibliev <samir.shibliev01@mail.ru>
2025-12-23 10:31:36 +0300

    reportWindow

---
+++ source/MessageBoxes/ReportWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Windows;
+
+namespace BIMPlugins.ExtStorage.MessageBoxes
+{
+    /// <summary>
+    /// Логика взаимодействия для ReportWindow.xaml
+    /// </summary>
+    public partial class ReportWindow : Window
+    {
+        public ReportWindow(IEnumerable collection)
+        {
+            InitializeComponent();
+            Box.ItemsSource = collection;
+        }
+    }
+}
