d3f953438f3ee074d35d98c7ab3557e72b009722
Samir Shibliev <samir.shibliev01@mail.ru>
2025-12-23 13:17:01 +0300

    availability

---
+++ source/Interfaces/CommandAvailability.cs
@@ -0,0 +1,20 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace BIMPlugins.ExtStorage.Interfaces
+{
+    public class AlwaysAvailable : IExternalCommandAvailability
+    {
+        public bool IsCommandAvailable(UIApplication applicationData, CategorySet selectedCategories) => applicationData != null;
+    }
+
+    public class NotAvailableInFamilyEditor : IExternalCommandAvailability
+    {
+        public bool IsCommandAvailable(UIApplication applicationData, CategorySet selectedCategories)
+        {
+            var doc = applicationData.ActiveUIDocument?.Document;
+            return doc != null && !doc.IsFamilyDocument;
+        }
+    }
+}
+
