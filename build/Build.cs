3188aed5d77133709c1a53b6fbf311ffdc48b4d9
Samir Shibliev <samir.shibliev01@mail.ru>
2026-03-06 12:23:19 +0300

    обновлен nuke

--- build/Build.cs
+++ build/Build.cs
@@ -10,13 +10,10 @@ public class Build : BIMPluginsBuild
 
 
     [Solution("BIMPlugins.ExtStorage.sln")]
-    public override Solution Solution { get; }
+    protected override Solution Solution { get; }
 
-    [Parameter("Project to build name")]
-    public override string ProjectName { get; } = "BIMPlugins.ExtStorage";
-
-    [Parameter("Build configurations to run sequentially")]
-    public override string[] Configurations { get; } = new[] { "R2019", "R2020", "R2021", "R2022", "R2023" };
+    [Parameter("Имя проекта для сборки")]
+    protected override string ProjectName { get; } = "BIMPlugins.ExtStorage";
 
     public static int Main() => Execute<Build>(x => x.Compile);
 }
