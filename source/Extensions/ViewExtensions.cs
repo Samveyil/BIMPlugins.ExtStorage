a91521c326184217bb8f17ed309f520182fb5321
Samir Shibliev <samir.shibliev01@mail.ru>
2026-08-25 16:19:47 +0300

    new ViewExtension

---
+++ source/Extensions/ViewExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Linq;
+
+namespace BIMPlugins.ExtStorage.Extensions
+{
+    public static class ViewExtensions
+    {
+        /// <summary>Returns the UI view that corresponds to the specified view.</summary>
+        /// <returns>A <see cref="Autodesk.Revit.UI.UIView"/> with the same ViewId, or <see langword="null"/> if the view is not open.</returns>
+        public static UIView ToUIView(this View view)
+        {
+            return RevitAPI.UIDocument.GetOpenUIViews()
+                .FirstOrDefault(v => v.ViewId.ToString() == view.Id.ToString());
+        }
+    }
+}
